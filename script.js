a80c3500969a20258e9d77382dc4b5a57d25322b
Thomas, Samuel <samuelthomas2@my.unt.edu>
2024-08-06 01:06:59 -0500

    deleted:    dist/script.js 	renamed:    dist/index.html -> index.html 	renamed:    dist/logs/aion.txt -> logs/aion.txt 	renamed:    dist/logs/exalted.txt -> logs/exalted.txt 	renamed:    dist/logs/test.txt -> logs/test.txt 	renamed:    dist/misc/ambient.mp3 -> misc/ambient.mp3 	new file:   script.js 	renamed:    dist/styles.css -> styles.css

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var audio = document.getElementById("backgroundMusic");
+    audio.volume = 0.1; // Lower volume
+    audio.play();
+
+    const passwordForm = document.getElementById('passwordForm');
+    const passwordInput = document.getElementById('passwordInput');
+    const logContent = document.getElementById('logContent');
+
+    passwordForm.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const password = passwordInput.value.trim();
+        const logsUrl = 'logs/' + password + '.txt'; // Adjusted path to logs
+
+        try {
+            const response = await fetch(logsUrl);
+            if (response.ok) {
+                const text = await response.text();
+                logContent.style.display = 'block';
+                logContent.textContent = text;
+            } else {
+                logContent.style.display = 'none';
+                alert("Logs not found");
+                setTimeout(() => logContent.style.display = 'none', 3000); // Hide message after 3 seconds
+            }
+        } catch (error) {
+            console.error('Error fetching the log:', error);
+            alert("Error fetching the log");
+            setTimeout(() => logContent.style.display = 'none', 3000); // Hide message after 3 seconds
+        }
+    });
+});
