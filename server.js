0896169126f1f1eb9700c17255a801bd04d51c82
Thomas, Samuel <samuelthomas2@my.unt.edu>
2024-08-05 23:22:14 -0500

    modified:   server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/misc', express.static(path.join(__dirname, 'misc')));
 
 // Serve log files dynamically
